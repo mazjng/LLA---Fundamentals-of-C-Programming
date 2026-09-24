570a4d07ec8b638973942e92755ebc8ea0c91f88
marcus@arch <marcusngzhijie@gmail.com>
2026-09-24 18:53:41 +0800

    kv_delete

--- src/kv.c
+++ src/kv.c
@@ -17,6 +17,42 @@ size_t hash(char *val, int capacity) {
     return hash % capacity;
 }
 
+// fn kv_delete
+// params:
+//  - db: a pointer to db
+//  - key: a pointer to key to delete
+//  returns: 0 on success, -1 on error
+    
+int kv_dete(kv_t *db, char* key) {
+    if (!db || !key) return -1;
+
+    size_t idx = hash(key, db->capacity);
+
+    for(int i = 0; i < db->capacity; i++) {
+        
+        size_t seek_idx = (idx + i) % db->capacity;
+        kv_entry_t *entry = &db->entries[seek_idx];
+
+        if (entry->key == NULL) {
+            return -1;
+        }
+
+        if (entry->key
+            && entry->key != TOMBSTONE
+            && !strcmp(entry->key, key)) {
+            free(entry->key);
+            free(entry->value);
+
+            db->count --;
+
+            entry->key = TOMBSTONE;
+            entry->value = NULL;
+        }
+    }
+
+    return -1;
+}
+
 // fn kv_get
 // params:
 //  - db: a pointer to db
--- src/main.c
+++ src/main.c
@@ -14,5 +14,11 @@ int main() {
     char *val2 = kv_get(table, "lala");
     char *val3 = kv_get(table, "dosent exist");
 
-    printf("%s | %s | %s |", val, val2, val3);
+    printf("%s | %s | %s \n", val, val2, val3);
+    
+    kv_dete(table, "hehe");
+    val = NULL;
+    val = kv_get(table, "hehe");
+
+    printf("%s | %s | %s \n", val, val2, val3);
 }
